c9f281aff477c98da001b0c3b79b9d73108f3f0b
Ngahu <jamaalaraheem@gmail.com>
2017-11-06 23:12:26 +0300

    creating of the usermanager

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -3,9 +3,47 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import (
-    AbstractBaseUser
+    AbstractBaseUser,
+    BaseUserManager
 )
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self,email,password=None,is_active=True,is_staff=False,is_admin=False):
+        if not email:
+            raise ValueError("Users Must have an email Adress!")
+            if not password:
+                raise ValueError("User Must Have a Password!!")
+        user_obj = self.model(
+            email = self.normalize_email(email)
+
+        )
+        user_obj.set_password(password)
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.active = is_active
+        user_obj.save(using=self._db)
+        return user_obj 
+
+    def create_staffuser(self,email,password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            is_staff=True
+        )
+        return user
+
+
+    def create_superuser(self,email,password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            is_staff=True,
+            is_admin=True
+        )
+        return user
+
+
 
 
 class User(AbstractBaseUser):
@@ -15,6 +53,8 @@ class User(AbstractBaseUser):
     admin     =     models.BooleanField(default=False) #super user 
     timestamp =     models.DateTimeField(auto_now_add=True)
 
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
