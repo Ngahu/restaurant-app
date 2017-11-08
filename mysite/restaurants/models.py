ff8588f10a89cfaa70bcbbb8e9445ccf34c5cc8c
Ngahu <jamaalaraheem@gmail.com>
2017-11-08 23:57:00 +0300

    creating of the utils file that the function that is responsible to create a unique random slug for every new title added in the db

--- mysite/restaurants/models.py
+++ mysite/restaurants/models.py
@@ -5,15 +5,20 @@ from django.db import models
 
 # Create your models here.
 class RestaurantLocation(models.Model):
-    name = models.CharField(max_length=120)
-    location = models.CharField(max_length=120,blank=True, null=True)
-    category = models.CharField(max_length=120,blank=False, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True)  #when it was created/ added
-    updated = models.DateTimeField(auto_now=True) #when it was updated/changed
+    name      =      models.CharField(max_length=120)
+    location  =      models.CharField(max_length=120,blank=True, null=True)
+    category  =      models.CharField(max_length=120,blank=False, null=True)
+    timestamp =      models.DateTimeField(auto_now_add=True)  #when it was created/ added
+    updated   =      models.DateTimeField(auto_now=True) #when it was updated/changed
+    slug      =      models.SlugField(unique=True)
 
 
     def __str__(self):
         return self.name
 
     def __unicode__(self):
+        return self.name
+
+    @property
+    def title(self):
         return self.name
