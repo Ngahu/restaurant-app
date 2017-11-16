0ac6833861a43d2aedbbbf7f72c37b1cf7d2fb9d
Ngahu <jamaalaraheem@gmail.com>
2017-11-16 23:01:35 +0300

    creating of the menus item model and also creating of the two  methods that are going to return the contents and exclusions as comma separated

--- mysite/menus/models.py
+++ mysite/menus/models.py
@@ -2,5 +2,27 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
+from restaurants.models import RestaurantLocation
+
 
 # Create your models here.
+
+class Item(models.Model):
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL)
+    restaurant  = models.ForeignKey(RestaurantLocation)
+    name        = models.CharField(max_length=300)
+    contents    = models.TextField(help_text='Separate each item by Comma')
+    excludes    = models.TextField(blank=True, null=True,help_text='Separate each item by Comma')
+    public      = models.BooleanField(default=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)  #when it was created/ added
+    updated     = models.DateTimeField(auto_now=True) #when it was updated/changed
+
+    class Meta:
+        ordering = ['-updated','-timestamp']
+    
+    def get_contents(self):
+        return self.contents.split(",")
+
+    def get_excludes(self):
+        return self.excludes.split(",")
